b4c73e7f24a6767f3540edb50e40266203085da0
조현일 <himzei@gmail.com>
2023-07-29 18:10:47 +0900

    re

--- js/apiData.js
+++ js/apiData.js
@@ -1,16 +1,13 @@
 let addressArray = [];
 fetch(
-  "https://api.odcloud.kr/api/15052836/v1/uddi:2253111c-b6f3-45ad-9d66-924fd92dabd7?page=1&perPage=1000&serviceKey=I3ONCgSdg1Tf8GYukQdpJYSpq7nLY9Uqy3AE1Zp%2B1tx1r11%2BDj5djJKCEllItBS2WtgL318%2BX93ZNZ5K5RHSsA%3D%3D",
+  "https://port-0-foods-api-project-rt92alknmotf2.sel4.cloudtype.app/api/foods/joongu",
   {
     method: "GET",
     headers: {
       accept: "application/json",
-      Authorization:
-        "I3ONCgSdg1Tf8GYukQdpJYSpq7nLY9Uqy3AE1Zp%2B1tx1r11%2BDj5djJKCEllItBS2WtgL318%2BX93ZNZ5K5RHSsA%3D%3D",
     },
+    credentials: "include",
   }
 )
   .then((res) => res.json())
-  .then((data) => {
-    localStorage.setItem("addressArray", JSON.stringify(data));
-  });
+  .then((data) => console.log(data));
